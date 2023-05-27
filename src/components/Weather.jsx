40524c6a8fd11e151c0b118fbed80598c94e9eb7
Ayush Tiwari <ayushtiwari134@gmail.com>
2023-05-27 18:16:46 +0530

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+const Weather = () => {
+
+    const [data, setData] = useState({})
+    const [city, setCity] = useState("")
+    const [view, setView] = useState(false)
+
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=c68f31b9ff824075a8a101816232705&q=${city}`
+
+    const handleChange = (event) => {
+        setCity(event.target.value)
+    }
+
+
+
+    const handleClick = async () => {
+        await fetch(apiUrl).then(res => res.json()).then((result) => { setData(result); })
+        setCity("")
+        setView(true)
+    }
+
+
+    return (
+        <div className=" h-screen w-full flex flex-col justify-around items-center">
+            <div className="search-bar md:w-[50%] w-[100%] flex justify-center">
+                <input type='text' value={city} placeholder='Enter city' onChange={handleChange} className='p-2 border-black border mr-1' />
+                <button onClick={handleClick} className="border-2 ml-1 border-red-700 px-4 hover:bg-red-500 hover:text-white">Search</button>
+            </div>
+            {view && <div className="upper-part">
+
+                <div className="name m-4 text-center">
+                    {data.location !== undefined ? <h1 className='font-bold md:text-4xl text-3xl'>{data.location.name}</h1> : <p></p>}
+
+                </div>
+                <div className="temperature text-center m-4">
+                    {data.current !== undefined ? <h1 className="md:text-8xl text-6xl">{data.current.temp_c}&#8451;</h1> : <p></p>}
+
+                </div>
+            </div>}
+            {view && <div className="lower-part flex md:mb-0 mb-[23%] md:w-[70%] w-[100%] justify-center items-center">
+                <div className="div border-r-2 border-black px-5 text-center">
+                    {data.current !== undefined ? <h1 className="md:text-3xl text-xl font-bold">{data.current.wind_kph} kph</h1> : <p></p>}
+
+                    <p className="md:text-xl">Wind speed</p>
+                </div>
+                <div className="div border-r-2 border-black px-4 text-center">
+                    {data.current !== undefined ? <h1 className="md:text-3xl text-xl font-bold">{data.current.feelslike_c}&#8451;</h1> : <p></p>}
+                    <p className='md:text-xl'>Feels like</p>
+                </div>
+                <div className="div text-center px-5">
+                    {data.current !== undefined ? <h1 className="md:text-3xl text-xl font-bold">{data.current.condition.text}</h1> : <p></p>}
+                    <p>Type Of Day</p>
+                </div>
+            </div>}
+        </div>
+    )
+}
+
+export default Weather
